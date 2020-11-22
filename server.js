65244019bb48be031e6fdce000d349c637f692e2
ajeas <smithajea@rocketmail.com>
2020-11-22 09:46:48 -0500

    modified server.js file

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 app.get("*", (req, res) => {
-  res.send("api is running");
+  res.send("api is running now");
 });
 
 // Start server
